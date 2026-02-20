629340c5736f6243a482c3abd59c3344226223d9
lkbajdp <lkbajdp@gmail.com>
2026-02-20 21:30:20 +0300

    feat: add SceneLoader

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -5,5 +5,37 @@ using System.Collections;
 
 public class SceneLoader : MonoBehaviour
 {
-    
+    [SerializeField] private GameObject loadingScreen;
+    [SerializeField] private Slider progressBar;
+
+    public void LoadScene(string sceneName)
+    {
+        if (!Application.CanStreamedLevelBeLoaded(sceneName))
+        {
+            return;
+        }
+
+        StartCoroutine(LoadSceneAsync(sceneName));
+    }
+
+    private IEnumerator LoadSceneAsync(string sceneName)
+    {
+        if (loadingScreen != null)
+            loadingScreen.SetActive(true);
+
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+
+        while (!operation.isDone)
+        {
+            float progressValue = Mathf.Clamp01(operation.progress / 0.9f);
+
+            if (progressBar != null)
+                progressBar.value = progressValue;
+
+            yield return null;
+        }
+
+        if (loadingScreen != null)
+            loadingScreen.SetActive(false);
+    }
 }
