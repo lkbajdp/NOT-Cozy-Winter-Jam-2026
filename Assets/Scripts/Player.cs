5f5410cf0e4bf6b687576457424326f57756387f
lkbajdp <lkbajdp@gmail.com>
2026-02-25 12:45:31 +0300

    feat: add Player script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public static Player Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+}
