415e9e208cb212ee615b69c11426e101184c790e
lkbajdp <lkbajdp@gmail.com>
2026-02-25 13:17:37 +0300

    Update PlayerInputHandler.cs

--- Assets/Scripts/Player/PlayerInputHandler.cs
+++ Assets/Scripts/Player/PlayerInputHandler.cs
@@ -2,5 +2,17 @@ using UnityEngine;
 
 public class PlayerInputHandler : MonoBehaviour
 {
-    
+    public Vector2Int? GetMovementInput()
+    {
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+            return Vector2Int.up;
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+            return Vector2Int.down;
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+            return Vector2Int.left;
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+            return Vector2Int.right;
+
+        return null;
+    }
 }
