cfac07ba8da59be9766d5e63b3509b63b84ae7c4
lkbajdp <lkbajdp@gmail.com>
2026-02-26 11:20:46 +0300

    feat: add GridData to GridMaanger

--- Assets/Scripts/Grid/GridManager.cs
+++ Assets/Scripts/Grid/GridManager.cs
@@ -3,6 +3,11 @@ using UnityEngine;
 public class GridManager : MonoBehaviour
 {
     public static GridManager Instance { get; private set; }
+    public GridData Data { get; private set; }
+
+    [SerializeField] private int _width = 32;
+    [SerializeField] private int _height = 32;
+
 
     private void Awake()
     {
@@ -16,5 +21,7 @@ public class GridManager : MonoBehaviour
             Destroy(gameObject);
             return;
         }
+
+        Data = new GridData(_width, _height);
     }
 }
